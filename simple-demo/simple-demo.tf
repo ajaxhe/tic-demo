e066e9e091dac5d27aaff7ac3403f3101797b091
ajaxhe <ajaxhe@gmail.com>
2020-07-01 11:19:06 +0800

    modify

--- simple-demo/simple-demo.tf
+++ simple-demo/simple-demo.tf
@@ -13,6 +13,10 @@ variable "default_az" {
 }
 
 resource "tencentcloud_cos_bucket" "demo_cos" {
+  bucket = "ajaxhe-demo-1259649581"
+  acl    = "private"
+}
+resource "tencentcloud_cos_bucket" "demo_cos2" {
   bucket = "ajaxhe-demo2-1259649581"
   acl    = "private"
 }
